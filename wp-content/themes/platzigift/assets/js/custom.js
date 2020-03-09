b56fa2dab62d2bfe7c6544c9cd980a922dc15f11
Jose Monterrey <jomonter@ucab.edu.ve>
2020-03-09 14:55:14 -0400

    comienzo del ajax

--- wp-content/themes/platzigift/assets/js/custom.js
+++ wp-content/themes/platzigift/assets/js/custom.js
@@ -1,1 +1,11 @@
-console.log('Hola Wordpress')
+(function ($) {
+    console.log('Hola Wordpress')
+    $('#categorias-productos').change(function () {
+        $.ajax({
+            url:,
+            method: "POST",
+            data:,
+        })
+    })
+})(jQuery)
+
